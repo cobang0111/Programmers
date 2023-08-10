a925841f15d5bcb69e241d96986a5e0d45b3f6f7
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-08-10 16:30:38 +0900

    Create Lv.2_H-Index.py

---
+++ Lv.2_H-Index.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    answer = 0
+    
+    citations.sort(reverse = True)
+    max_h = citations[0]
+    
+    while True:
+        if max_h <= len(citations) and citations[max_h-1] >= max_h:
+            answer = max_h
+            break
+        else :
+            max_h -= 1
+        
+    return answer
