d8075c69fb08c3881a98f6029b450dee8c24721d
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-08-11 11:50:06 +0900

    코

---
+++ Lv.2_이중우선순위큐.py
@@ -0,0 +1,27 @@
+def solution(operations):
+    
+    queue = []
+    
+    for string in operations:
+        # Data slicing
+        string_list = string.split() 
+        order = string_list[0]
+        num = int(string_list[1])
+        # Case : Insert
+        if order == "I":
+            queue.append(num)
+        # Case : Delelte max from non-empty queue
+        elif num == 1 and queue:
+            queue.pop()
+        # Case : Delelte min from non-empty queue
+        elif num == -1 and queue:
+            queue.pop(0)
+        else:
+            continue
+        queue.sort()
+    
+    if queue:
+        answer = [queue[-1], queue[0]]
+    else:
+        answer = [0, 0]
+    return answer
